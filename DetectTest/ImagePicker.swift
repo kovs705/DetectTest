05be2b8bbe480b755652b7e75a4c2bdc9b7cb2f8
kovs705 <smartkovsik@gmail.com>
2021-06-10 18:12:03 +0300

    made a Coordinator for ImagePicker

--- DetectTest/ImagePicker.swift
+++ DetectTest/ImagePicker.swift
@@ -9,16 +9,33 @@ import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
     
+    @Binding var image: Image?
+    @Environment(\.presentationMode) var presentationMode
+    
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
         // code
     }
     
     // a view that respresents a UIKit controller:
-    
     func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
         
         let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
         return picker
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
         
     }
     
