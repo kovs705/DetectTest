ca8dd4dcf4f0f4df0708e0c0770f80dfc56bf54c
kovs705 <smartkovsik@gmail.com>
2021-06-10 18:17:54 +0300

    added ImagePickerController

--- DetectTest/ImagePicker.swift
+++ DetectTest/ImagePicker.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
     
-    @Binding var image: Image?
+    @Binding var image: UIImage?
     @Environment(\.presentationMode) var presentationMode
     
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
@@ -30,13 +30,20 @@ struct ImagePicker: UIViewControllerRepresentable {
     }
     
     class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-        
         var parent: ImagePicker
         
         init(_ parent: ImagePicker) {
             self.parent = parent
         }
         
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
     }
     
 }
